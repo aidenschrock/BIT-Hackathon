11adc6b94333997ce65e493adbea666bab487b7a
Joseph Ani <joeoani@gmail.com>
2022-01-30 16:28:48 -0500

    about

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Account from "./pages/account";
-import Page404 from "./pages/page404";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Account from './pages/account'
+import About from './pages/about'
+import Page404 from './pages/page404'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/BIT-Hackathon/" element={<App />} />
-        <Route path="/BIT-Hackathon/account" element={<Account />} />
-        <Route path="*" element={<Page404 />} />
+        <Route path='/BIT-Hackathon/' element={<App />} />
+        <Route path='/BIT-Hackathon/account' element={<Account />} />
+        <Route path='/BIT-Hackathon/about' element={<About />} />
+        <Route path='*' element={<Page404 />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
---
+++ src/pages/about.js
@@ -0,0 +1,42 @@
+import { Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
+
+function About() {
+  return (
+    <div style={style.container}>
+      <h2>About</h2>
+      <section>
+        <p>
+          This project was drawn from data collected on problems people face
+          living in shared spaces.
+        </p>
+        <h3>Team</h3>
+        <ul>
+          <li>Morgan</li>
+          <li>Meera</li>
+          <li>Aiden</li>
+          <li>Joe</li>
+        </ul>
+      </section>
+    </div>
+  )
+}
+
+const style = {
+  container: {
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    height: '400px',
+    margin: '2%',
+  },
+  link: {
+    textDecoration: 'none',
+  },
+}
+
+export default About
