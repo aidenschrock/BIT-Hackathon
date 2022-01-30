c8205717b629b978586ba0b15500353972d6fe20
Morgan Sherrill <mo.sherrill@outlook.com>
2022-01-29 20:57:11 -0800

    some changes to send password reset

--- src/context/userAuthContext.js
+++ src/context/userAuthContext.js
@@ -43,7 +43,7 @@ export function AuthProvider({ children }) {
   }
 
   function resetPassword(email) {
-    return auth.sendPasswordResetEmail(email)
+    return sendPasswordResetEmail(auth, email)
   }
 
   function updateUserEmail(email) {
